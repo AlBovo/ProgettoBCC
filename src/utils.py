949aaa0cd7bca8774db10e72408d83b765c89c8c
Marco Balducci <balduccimarco2007@gmail.com>
2024-03-30 12:51:28 +0100

    get_week_days now returns tuples
    format is (day, month, year)

--- src/utils.py
+++ src/utils.py
@@ -8,26 +8,39 @@ def is_valid_date(date_str):
     except ValueError:
         return False
     
-def get_week_days(day:int, month:int, year:int) -> list[int]:
+def get_week_days(day:int, month:int, year:int) -> list[tuple[int,int,int]]:
     dayWeek = datetime(year, month, day).isoweekday()
     MonthDays = calendar.monthrange(year=year, month=month)[1]
-    if month == 1:
-        PreviousMonthDays = calendar.monthrange(year=12, month=12)[1]
-    else: PreviousMonthDays = calendar.monthrange(year=year-1, month=month-1)[1]
+    if month == 1: 
+        PreviousMonth = 12
+        PreviousMonthYear = year - 1
+        NextMonthYear = year
+    else: 
+        PreviousMonth = month - 1
+        PreviousMonthYear = year
+        if month == 12: 
+            nextMonth = 1
+            nextMonthYear = year+1
+        else: 
+            nextMonth = month + 1
+            nextMonthYear = year
+
+
+    PreviousMonthDays = calendar.monthrange(year=year-1, month=PreviousMonth)[1]
 
     days = []
-    nextMonth = False
+    forwardMonth = False
 
     for i in range(7):
         temp = day+i-dayWeek+1
         if temp >= 0:
-            days.append( temp % (MonthDays+1))
-            if nextMonth or (days[i] == 0 and day > 7):
-                days[i] = days[i] % MonthDays + 1
-                nextMonth = True
-            elif days[i] == 0:
-                days[i] = PreviousMonthDays
+            days.append( (temp % (MonthDays+1), month, year))
+            if forwardMonth or (days[i][0] == 0 and day > 7):
+                days[i] = (days[i][0] % MonthDays + 1, nextMonth, nextMonthYear)
+                forwardMonth = True
+            elif days[i][0] == 0:
+                days[i] = (PreviousMonthDays, PreviousMonth, PreviousMonthYear)
         else:
-            days.append(PreviousMonthDays - abs(temp))
+            days.append((PreviousMonthDays - abs(temp), PreviousMonth, PreviousMonthYear))
 
     return days
