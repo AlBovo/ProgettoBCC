8b793396109835262730fb04f1309d6eb2bf3138
Marco Balducci <balduccimarco2007@gmail.com>
2024-03-30 12:30:01 +0100

    deleted useless prints in get_week_days

--- src/utils.py
+++ src/utils.py
@@ -11,7 +11,6 @@ def is_valid_date(date_str):
 def get_week_days(day:int, month:int, year:int) -> list[int]:
     dayWeek = datetime(year, month, day).isoweekday()
     MonthDays = calendar.monthrange(year=year, month=month)[1]
-    print(MonthDays, dayWeek)
     if month == 1:
         PreviousMonthDays = calendar.monthrange(year=12, month=12)[1]
     else: PreviousMonthDays = calendar.monthrange(year=year-1, month=month-1)[1]
@@ -21,7 +20,6 @@ def get_week_days(day:int, month:int, year:int) -> list[int]:
 
     for i in range(7):
         temp = day+i-dayWeek+1
-        print(temp)
         if temp >= 0:
             days.append( temp % (MonthDays+1))
             if nextMonth or (days[i] == 0 and day > 7):
