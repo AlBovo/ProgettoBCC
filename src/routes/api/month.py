eabb99c7d4df8118b4c8ef24fb8eae65eabe0d61
Marco Balducci <balduccimarco2007@gmail.com>
2024-03-30 10:39:39 +0100

    added getMonth

--- src/routes/api/month.py
+++ src/routes/api/month.py
@@ -1,5 +1,21 @@
-from flask import redirect, url_for
+from flask import redirect, url_for, request, jsonify
+import calendar
+from models import OperatorManager, Operator, EventManager, Event
 
 def get_month():
-    # TODO: add logic
-    return redirect(url_for('main.index')) # return data
+    #structure of post request: "operator":"operatorID", "month":"monthNumber"
+    data = request.json
+    operator = OperatorManager.get(data["operator"])
+    
+    if not Operator: return None #TODO: add error to user
+
+    response = []
+    for day in range(calendar.monthrange(year=2023, month=data["month"])):
+        events = operator.getEventsByDay(day=day) #returns a list of events in that day
+        
+        response.append({
+            "day":day,
+            "events":len(events)
+        })
+    
+    return jsonify(response)
