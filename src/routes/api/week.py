778e8ced5518cb1ae73fc491ecd1e5452c664fbb
Marco Balducci <balduccimarco2007@gmail.com>
2024-04-02 22:37:08 +0200

    Update week.py

--- src/routes/api/week.py
+++ src/routes/api/week.py
@@ -1,16 +1,17 @@
 from flask import redirect, url_for, request, jsonify
 import calendar
 from models import OperatorManager, Operator, EventManager, Event
+from utils import get_week_days
 
 def get_month():
-    #structure of post request: "operator":"operatorID", "month":"monthNumber"
+    #structure of post request: "operator":"operatorID", "day":"dayNumber", "month":"monthNumber", "year":"yearNumber"
     data = request.json
     operator = OperatorManager.get(data["operator"])
     
     if not Operator: return None #TODO: add error to user
 
     response = []
-    for day in range(calendar.monthrange(year=2023, month=data["month"])):
+    for day in range(get_week_days(day=data["day"], month=data["month"], year=data["year"])):
         events = operator.getEventsByDay(day=day) #returns a list of events in that day
         
         response.append({
