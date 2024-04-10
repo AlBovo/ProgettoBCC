9d8e9ce6e8e4eb5fcb03dbe598f02c7f1baf5b3b
Simone Ceccarelli <Simonececcarelli001@gmail.com>
2024-04-10 22:02:56 +0200

    calendar base template
    Not finished

--- src/website/render.py
+++ src/website/render.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def login():
   return render_template('login.html')
 
+
+@app.route('/index')
+def index():
+  return render_template('index.html')
+
 @app.route('/register')
 def register():
   return render_template('register.html')
