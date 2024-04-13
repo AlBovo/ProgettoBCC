607491c00125da3ae8c04d6c56b4dc511af6eace
Alan Davide Bovo <alandavide.bovo.stud@ispascalcomandini.it>
2024-04-13 17:06:33 +0200

    templates creation simplified

--- src/static/login.js
+++ src/static/login.js
@@ -1,26 +1,27 @@
+const imagesNumber = 3;
 var previousIndex = 1;
 
 function getRandomImage() {
-  var index = Math.floor(Math.random() * images.length);
-  while (index === previousIndex) {
-    index = Math.floor(Math.random() * images.length);
+  var index = Math.floor(Math.random() * imagesNumber) + 1;
+  if (index === previousIndex) {
+    index = (index) % imagesNumber + 1;
   }
-  index++;
   previousIndex = index;
   return index;
 }
 
 function changeBackground() {
+  var prevBackgroundImage = document.getElementById(`background-image-${previousIndex}`);
   var newImage = getRandomImage();
-  var prevBackgroundImage = document.getElementById('background-image${previousIndex}');
-  var backgroundImage = document.getElementById('background-image${newImage}');
+  var backgroundImage = document.getElementById(`background-image-${newImage}`);
 
+  prevBackgroundImage.removeAttribute("style");
   prevBackgroundImage.hidden = true;
+
   backgroundImage.hidden = false;
   backgroundImage.style.opacity = 0;
 
   setTimeout(function() {
-    backgroundImage.src = newImage;
     backgroundImage.style.opacity = 1;
   }, 500); 
 }
---
+++ src/static/operator.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    //page loaded
+    var parent = document.getElementById('generics');
+
+    //TODO: al load della pagina richiedere le generalità dell'operatore e aggiungerle
+    //parent.innerHTML += ``
+})
