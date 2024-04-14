87e6559cdb42be4f7ffed20df7cc156c1c1abd9b
Alan Davide Bovo <alandavide.bovo.stud@ispascalcomandini.it>
2024-04-14 20:54:48 +0200

    added flash modal for alerts

--- src/render.py
+++ src/render.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 
 app = Flask(__name__)
 app.config['FRONTEND'] = True
+app.config['SECRET_KEY'] = 'gasighoagohsaighaoigshaisgh'
 
 @app.route('/login')
 def login():
   return render_template('login.html')
 
+@app.route('/logout')
+def logout():
+  flash('You have been logged out!', 'success')
+  return redirect(url_for('login'))
 
 @app.route('/index')
 def index():
--- src/static/login.js
+++ src/static/login.js
@@ -28,3 +28,13 @@ function changeBackground() {
 }
 
 setInterval(changeBackground, 10000);
+
+window.openModal = function(modalId) {
+  document.getElementById(modalId).style.display = 'block'
+  document.getElementsByTagName('body')[0].classList.add('overflow-y-hidden')
+}
+
+window.closeModal = function(modalId) {
+  document.getElementById(modalId).style.display = 'none'
+  document.getElementsByTagName('body')[0].classList.remove('overflow-y-hidden')
+}
