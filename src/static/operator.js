3c345d89514a88557cac35d4e4eac73e05996ed6
Marco Balducci <balduccimarco2007@gmail.com>
2024-04-15 17:28:48 +0200

    operator.html modified

--- src/static/operator.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    //page loaded
-    var parent = document.getElementById('generics');
-
-    //TODO: al load della pagina richiedere le generalità dell'operatore e aggiungerle
-    //parent.innerHTML += ``
-})
