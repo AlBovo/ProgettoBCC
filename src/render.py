a41ce1b1f427e89bfb046c1a855df5809c0608d9
Alan Davide Bovo <alandavide.bovo.stud@ispascalcomandini.it>
2024-04-15 20:41:51 +0200

    added error page and fixed some bugs

--- src/render.py
+++ src/render.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, url_for, flash, redirect, Blueprint
+from flask_wtf import CSRFProtect
 
 app = Flask(__name__)
 app.config['FRONTEND'] = True
 app.config['SECRET_KEY'] = 'gasighoagohsaighaoigshaisgh'
+csrf = CSRFProtect(app)
 api = Blueprint('api', __name__)
+main = Blueprint('main', __name__)
 
 @api.route('/login', methods=['POST'])
 def login():
@@ -11,36 +14,41 @@ def login():
   return redirect(url_for('login'))
 
 @api.route('/logout', methods=['POST'])
-def apiLogout():
-  flash('You have been logged out!', 'success')
-  return redirect(url_for('login'))
-
-@api.route('/register', methods=['POST'])
-def apiRegister():
-  flash('You have been registered!', 'success')
-  return redirect(url_for('register'))
-
-@app.route('/login')
-def login():
-  return render_template('login.html', login=True)
-
-@app.route('/logout')
 def logout():
   flash('You have been logged out!', 'success')
   return redirect(url_for('login'))
 
-@app.route('/')
+@api.route('/register', methods=['POST'])
+def register():
+  flash('You have been registered!', 'success')
+  return redirect(url_for('register'))
+
+@main.route('/login')
+def login():
+  return render_template('login.html', login=True)
+
+@main.route('/logout')
+def logout():
+  flash('You have been logged out!', 'success')
+  return redirect(url_for('login'))
+
+@main.route('/')
 def index():
   return render_template('index.html')
 
-@app.route('/register')
+@main.route('/register')
 def register():
   return render_template('login.html', login=False)
   
-@app.route('/operator')
+@main.route('/operator')
 def operator():
   return render_template('operator.html')
 
+@main.route('/error')
+def error():
+  return render_template('error.html')
+
 if __name__ == '__main__':
   app.register_blueprint(api, url_prefix='/api')
+  app.register_blueprint(main, url_prefix='/')
   app.run('0.0.0.0', debug=True)
