217d5b4b8aa4a561459b9df81045be9818014b59
Alan Davide Bovo <alandavide.bovo.stud@ispascalcomandini.it>
2024-04-25 20:26:58 +0200

    fixed get day - TO TEST

--- src/routes/api/day.py
+++ src/routes/api/day.py
@@ -1,16 +1,21 @@
 from flask import request, jsonify
-from models import OperatorManager
+from models import EventManager
 from datetime import datetime
 
 def get_day():
+    """
+    Retrieves the events for a specific day and operator.
+    Post request with: "date":YYYY-MM-GG
     
-    # Post request with: "date":YYYY-MM-GG, "operator":operatorId
-
+    Returns:
+        A JSON response containing the start and end hours of each event for the specified day and operator.
+        If there are any errors, an error message is returned with an appropriate status code.
+    """
+    
     data = request.json()
 
-    if not data["date"] or not data["operator"]: return jsonify({'error': 'Missing parameters'}), 400
+    if not data["date"]: return jsonify({'error': 'Missing parameters'}), 400
     if not isinstance(data["date"], str): return jsonify({'error': 'Invalid date'}), 400
-    if not isinstance(data["operator"], int): return jsonify({'error' : 'Invalid operator'}), 400
 
     #check invalid date (date is in the past)
     try:
@@ -19,15 +24,23 @@ def get_day():
     except:
         return jsonify({'error': 'Invalid format date'}), 400
     
-    operator = OperatorManager.get(data["operator"])
-    events = operator.getEventsByDate(data["date"])
-
-    response = []
-
-    for event in events:
-        response.append({
-            "start_hour": event.getTimeSpan()[0],
-            "end_hour"  : event.getTimeSpan()[1]
-        })
+    events = [
+        {"start_hour": 0, "end_hour": 830}, # not avaible before 8:30 
+        EventManager.getEventsByDate(data["date"]),
+        {"start_hour": 1700, "end_hour": 2400} # not avaible after 17:00
+    ]
     
+    response = []
+    
+    hour, e = events[0]["end_hour"], 1
+    while hour < events[-1]["start_hour"]:
+        if hour == events[e]["start_hour"]:
+            hour = events[e]["end_hour"]
+            e += 1
+        else:
+            response.append({
+                "start_hour": hour,
+                "end_hour"  : (hour := (hour if hour % 100 < 60 else (hour // 100 + 1) * 100))
+            })
+            
     return jsonify(response), 200
