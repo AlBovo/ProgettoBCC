19a388cd49df105211e669c5b6b6a4f6e6828f97
Marco Balducci <balduccimarco2007@gmail.com>
2024-04-26 21:26:29 +0200

    added operator_stats

--- src/routes/admin/__init__.py
+++ src/routes/admin/__init__.py
@@ -1,7 +1,9 @@
 from flask_login import login_required
 from flask import Blueprint
 from . import events
+from . import stats
 
 adminbp  =   Blueprint('admin', __name__)
 
 adminbp.route('/events', methods=['GET'])(login_required(events.get_operator_day))
+adminbp.route('/stats', methods=['POST'])(login_required(stats.operator_stats))
---
+++ src/routes/admin/stats.py
@@ -0,0 +1,31 @@
+from flask import flash, render_template, redirect, url_for
+from flask_login import current_user
+from models import OperatorManager
+import json
+
+def operator_stats():    
+    """
+    Retrieves the stats of the events of the operator
+
+    Returns:
+        The stats representing the percentage of events in the morning and in the afternoon 
+    """
+  
+    if not current_user.get_privilege():
+        flash("You are not an operator at the moment", "error")
+        return redirect(url_for('main.login'))
+    
+    operator = OperatorManager().get(current_user.get_id())
+
+    morning = 0
+    afternoon = 0
+    events = operator.getAllEvents()
+
+    for event in events:
+        if event.getTimeSpan() < 1200:
+            morning += 1
+        else:
+            afternoon += 1
+    
+    return json({'morning' : f'{morning/len(events):.2f}', 'afternoon': f'{afternoon/len(events):.2f}'}), 200
+        
