f61edc14e367da2d875d26c8b10562637a785375
AlBovo <alandavide.bovo.stud@ispascalcomandini.it>
2024-05-20 13:18:38 +0200

    fixed leaks of past data and fixed format of timespans

--- src/routes/admin/events.py
+++ src/routes/admin/events.py
@@ -2,6 +2,7 @@ from flask import flash, render_template, redirect, url_for
 from flask_login import current_user
 from models import OperatorManager
 from utils import code_to_category
+from datetime import datetime
 
 def get_operator_day():    
     """
@@ -22,13 +23,16 @@ def get_operator_day():
     for event in operator.getAllEvents():
         category = code_to_category(event.getCategory())
         timeSpan = event.getTimeSpan()
+        
+        if datetime.now().date() != datetime.strptime(event.getDate(), "%Y-%m-%d").date():
+            continue
 
         response.append({
             "date"      : event.getDate(),
             "email"     : event.getUser().get_email(),
             "category"  : category,
-            "start_hour": timeSpan[1],
-            "end_hour"  : timeSpan[2]
+            "start_hour": f"{timeSpan[1] // 100}:{timeSpan[1] % 100}",
+            "end_hour"  : f"{timeSpan[2] // 100}:{timeSpan[2] % 100}"
         })
     
     return render_template('operator.html', events=response)
